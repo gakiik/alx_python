4a34c7dcfe2f2531967c2313e703b4736e069fa9
Gakii <gakii.kkirimi@gmail.com>
2023-07-28 14:39:52 +0300

    1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     args_list = sys.argv[1:]
 
+    print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''))
     if num_args == 0:
-        print("{} arguments.".format(num_args))
+        print(".")
     else:
-        print("{} argument{}.".format(num_args, 's' if num_args != 1 else ''))
         for i, arg in enumerate(args_list, start=1):
             print("{}: {}".format(i, arg))
