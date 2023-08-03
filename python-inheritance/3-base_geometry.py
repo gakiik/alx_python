6216e2d542a03ed72cc22ac6feeacb1c0a55b40a
Gakii <gakii.kkirimi@gmail.com>
2023-08-03 22:01:33 +0300

    Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+3-base_geometry module
+
+Define an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing BaseGeometry.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
