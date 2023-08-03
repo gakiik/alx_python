e2351d7f48ec79885b3a79572c58c05c70126eb6
Gakii <gakii.kkirimi@gmail.com>
2023-08-03 21:24:43 +0300

    Only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+2-inherits_from module
+
+Define a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare the object against.
+
+    Returns:
+        True if the object is an instance of a class that inherited (directly or indirectly) from the specified class, otherwise False.
+    """
+    return isinstance(obj, type) and issubclass(obj, a_class) and type(obj) is not a_class
+
+
+if __name__ == "__main__":
+    a = True
+    if inherits_from(a, int):
+        print("{} inherited from class {}".format(a, int.__name__))
+    if inherits_from(a, bool):
+        print("{} inherited from class {}".format(a, bool.__name__))
+    if inherits_from(a, object):
+        print("{} inherited from class {}".format(a, object.__name__))
